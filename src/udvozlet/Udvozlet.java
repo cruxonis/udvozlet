f085b59f9d56ae253b1557c98d3ef9232c85fcca
cruxonis <43477142+cruxonis@users.noreply.github.com>
2018-10-05 18:18:45 +0200

    Update Udvozlet.java

--- src/udvozlet/Udvozlet.java
+++ src/udvozlet/Udvozlet.java
@@ -6,7 +6,7 @@ public class Udvozlet {
 
     
     public static void main(String[] args) {
-        Ember no=new Ember("Kati");
+        Ember no=new Ember("Gandalf");
         Ember ferfi=new Ember("Jancsi");
         
         System.out.println(no.koszont());
