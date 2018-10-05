a58b366cd71a08dd2f88d8233c0159f6695e2ff8
t1 <t1@122HP5>
2018-10-05 18:10:15 +0200

    első változat oszallyal

--- src/udvozlet/Udvozlet.java
+++ src/udvozlet/Udvozlet.java
@@ -6,7 +6,11 @@ public class Udvozlet {
 
     
     public static void main(String[] args) {
-        System.out.println("CSÁÁÁÁH!");
+        Ember no=new Ember("Kati");
+        Ember ferfi=new Ember("Endre");
+        
+        System.out.println(no.koszont());
+        System.out.println(ferfi.koszont());
     }
     
 }
